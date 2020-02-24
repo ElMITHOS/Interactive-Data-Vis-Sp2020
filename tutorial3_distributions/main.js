e0a38ef77d52281e33a4cd2589623edc64ca48b2
ElMITHOS <elmerhomero0007.em@gmail.com>
2020-02-23 21:14:22 -0500

    Tutorial 3 data issues

--- tutorial3_distributions/main.js
+++ tutorial3_distributions/main.js
@@ -1,4 +1,8 @@
-/**
+/**This graph Would have the "Hour" on the yAxis and the Number of "Crashes" on the xAxis,
+ * (the sum of the Count during an hour).
+ *  * The dropdown should show the list of the different causes of crashes.
+ * The viewer would then be able to see at what time certain causes of crashes occured.
+ * Then add another dropdown to select a different date or a different borough too* 
  * CONSTANTS AND GLOBALS
  * */
 const width = window.innerWidth * 0.7,
@@ -18,13 +22,13 @@ let yScale;
  * */
 let state = {
   data: [],
-  selectedParty: "All",
+  selectedBorough: "All",
 };
 
 /**
  * LOAD DATA
  * */
-d3.json("../data/environmentRatings.json", d3.autoType).then(raw_data => {
+d3.json("ValentinesNYC2020.csv", d3.autoType).then(raw_data => {
   console.log("raw_data", raw_data);
   state.data = raw_data;
   init();
@@ -38,12 +42,12 @@ function init() {
   // SCALES
   xScale = d3
     .scaleLinear()
-    .domain(d3.extent(state.data, d => d.ideology_rating))
+    .domain(d3.extent(state.data, d => d.Count))
     .range([margin.left, width - margin.right]);
 
   yScale = d3
     .scaleLinear()
-    .domain(d3.extent(state.data, d => d.environmental_rating))
+    .domain(d3.extent(state.data, d => d.CrashTime))
     .range([height - margin.bottom, margin.top]);
 
   // AXES
@@ -54,17 +58,17 @@ function init() {
   // add dropdown (HTML selection) for interaction
   // HTML select reference: https://developer.mozilla.org/en-US/docs/Web/HTML/Element/select
   const selectElement = d3.select("#dropdown").on("change", function() {
-    console.log("new selected party is", this.value);
+    console.log("new selected borough is", this.value);
     // `this` === the selectElement
     // this.value holds the dropdown value a user just selected
-    state.selectedParty = this.value;
+    state.selectedBorough = this.value;
     draw(); // re-draw the graph based on this new selection
   });
 
   // add in dropdown options from the unique values in the data
   selectElement
     .selectAll("option")
-    .data(["All", "D", "R", "I"]) // unique data values-- (hint: to do this programmatically take a look `Sets`)
+    .data(["All", "MANHATTAN", "QUEENS", "STATEN ISLAND", "BROOKLYN", "BRONX", "Unspecified"]) // unique data values-- (hint: to do this programmatically take a look `Sets`)
     .join("option")
     .attr("value", d => d)
     .text(d => d);
@@ -86,7 +90,7 @@ function init() {
     .attr("class", "axis-label")
     .attr("x", "50%")
     .attr("dy", "3em")
-    .text("Ideology Rating");
+    .text("Crash Time");
 
   // add the yAxis
   svg
@@ -99,7 +103,7 @@ function init() {
     .attr("y", "50%")
     .attr("dx", "-3em")
     .attr("writing-mode", "vertical-rl")
-    .text("Environmental Rating");
+    .text("Count");
 
   draw(); // calls the draw function
 }
@@ -112,13 +116,13 @@ function draw() {
   // filter the data for the selectedParty
   let filteredData = state.data;
   // if there is a selectedParty, filter the data before mapping it to our elements
-  if (state.selectedParty !== "All") {
-    filteredData = state.data.filter(d => d.party === state.selectedParty);
+  if (state.selectedBorough !== "All") {
+    filteredData = state.data.filter(d => d.Borough === state.selectedBorough);
   }
 
   const dot = svg
     .selectAll(".dot")
-    .data(filteredData, d => d.name) // use `d.name` as the `key` to match between HTML and data elements
+    .data(filteredData, d => d.Code) // use `d.name` as the `key` to match between HTML and data elements
     .join(
       enter =>
         // enter selections -- all data elements that don't have a `.dot` element attached to them yet
@@ -128,19 +132,22 @@ function draw() {
           .attr("stroke", "lightgrey")
           .attr("opacity", 0.5)
           .attr("fill", d => {
-            if (d.party === "D") return "blue";
-            else if (d.party === "R") return "red";
-            else return "purple";
+            if (d.Borough === "MANHATTAN") return "blue";
+            else if (d.Borough === "QUEENS") return "purple";
+            else if (d.Borough === "STATEN ISLAND") return "green";
+            else if (d.Borough === "BROOKLYN") return "red";
+            else if (d.Borough === "BRONX") return "orange";
+            else return "gray";
           })
           .attr("r", radius)
-          .attr("cy", d => yScale(d.environmental_rating))
+          .attr("cy", d => yScale(d.CrashTime))
           .attr("cx", d => margin.left) // initial value - to be transitioned
           .call(enter =>
             enter
               .transition() // initialize transition
-              .delay(d => 500 * d.ideology_rating) // delay on each element
+              .delay(d => 500 * d.Count) // delay on each element
               .duration(500) // duration 500ms
-              .attr("cx", d => xScale(d.ideology_rating))
+              .attr("cx", d => xScale(d.Count))
           ),
       update =>
         update.call(update =>
@@ -158,7 +165,7 @@ function draw() {
           // exit selections -- all the `.dot` element that no longer match to HTML elements
           exit
             .transition()
-            .delay(d => 50 * d.ideology_rating)
+            .delay(d => 50 * d.Count)
             .duration(500)
             .attr("cx", width)
             .remove()
